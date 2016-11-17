15e04f4ffdfac13361f9d49d5d4dc701847138b8
Yingjie <1275963@gmail.com>
2016-11-17 13:43:30 -0500

    A better testing script with subplot

--- test_script.m
+++ test_script.m
@@ -1,23 +1,35 @@
 % For N >= 5
 % Get input points (xy) 
 clear
-hold on
 
 init_point = 7;
-axis([0,1,0,1])
 
+figure()
+hold on
+axis([0,1,0,1])
 for i = 1:init_point
      [xy(1,i), xy(2,i)] = ginput(1);    
      plot(xy(1,:),xy(2,:), 'b*'); 
 end
-% Use different end condition to calculate control points
-dpoints = con4(xy);
-
 % Use the script to plot the bazier curve
 % scatter(xy(1,:), xy(2,:),'r*')
 % scatter(dpoints(1,:), dpoints(2,:), 'ko')
-
+figure()
+subplot(2,2,1);
+scatter(xy(1,:), xy(2,:),'b*')
 plot_bazier(con1(xy), 'r');
+title('end condition 1');
+subplot(2,2,2);
+scatter(xy(1,:), xy(2,:),'b*')
 plot_bazier(con2(xy), 'g');
+title('end condition 2');
+
+subplot(2,2,3);
+scatter(xy(1,:), xy(2,:),'b*')
 plot_bazier(con3(xy), 'b');
+title('end condition 3');
+
+subplot(2,2,4);
+scatter(xy(1,:), xy(2,:),'b*')
 plot_bazier(con4(xy), 'y');
+title('end condition 4');
